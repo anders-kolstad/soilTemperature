1f22592681b5c95fa516b9c2e1a24283cb0c7780
ANDERS L KOLSTAD <anders.kolstad@ntnu.no>
2017-11-22 15:48:39 +0100

    still struggeling with 'invalid parent values'

--- PI_calibration.R
+++ PI_calibration.R
@@ -30,6 +30,7 @@ library(readxl)
 library(dplyr)
 library(lattice)  #Needed for multi-panel graphs
 library(R2jags)
+library(ggplot2)
 setwd("M:\\Anders L Kolstad\\HIGHSTATS\\AllData")
 source(file = "HighstatLibV10.R")  
 source(file = "MCMCSupportHighstatV4.R")
@@ -163,7 +164,7 @@ boxplot(dat2$within_moss_DW_gmsq, main = "Within moss", xlab = "Total DW", ylab 
 
 # BLG ####
 par(mfrow = c(1,1), mar = c(5,5,7,2))
-plot(dat2$BLG_DW_g, dat2$Broad_leaved_grasses,  main = "BLG", xlab = "", ylab = "", pch = 20) # saturates - non-linear
+plot(dat2$Broad_leaved_grasses, dat2$BLG_DW_g,  main = "BLG",  pch = 20) # saturates - non-linear
 
 # Strictly positive values. Increasing variation. 
 # -> GLM, gamma distribution with identiy link
@@ -197,28 +198,32 @@ plot(dat2$BLG_DW_g, dat2$Broad_leaved_grasses,  main = "BLG", xlab = "", ylab = 
 # Step 0. Get data
 BLG <- filter(dat2, Broad_leaved_grasses != 0 & BLG_DW_g != 0)
 
-# scale continous covariates (can you do that with gamma?)
 
-#BLG$std.BLG_DW <- scale(BLG$BLG_DW_g)
+
+# scale continous covariates
 BLG$std.Broad_leaved_grasses <- scale(BLG$Broad_leaved_grasses)
-
-summary(BLG$std.BLG_DW) # OK
-sd(BLG$std.BLG_DW)      # OK
+# center continous covariates
+BLG$c.Broad_leaved_grasses <- BLG$Broad_leaved_grasses-mean(BLG$Broad_leaved_grasses)
+# tidy dtaframe
+BLG <- select(BLG, DW = BLG_DW_g, 
+              c.Hits = c.Broad_leaved_grasses,
+              std.Hits =std.Broad_leaved_grasses)
 
 
 
 #1. Bundle data
 
-X_BLG <- model.matrix(~ std.Broad_leaved_grasses , data = BLG)                      
+X_BLG <- model.matrix(~ c.Hits , data = BLG)   # keeping the intercept, but remember model is only valid of #hits >0
 K <- ncol(X_BLG)  
 head(X_BLG)
+K
 
 #Random effects:
 #None
 
 
 # Step 2: Prepare data for JAGS
-JAGS.data <- list(Y    = BLG$BLG_DW_g, 
+JAGS.data <- list(Y    = BLG$DW, 
                  X    = X_BLG,
                  N    = nrow(BLG),
                  K    = K       )
@@ -227,47 +232,48 @@ JAGS.data
 
 
 # Step 3: JAGS modelling code
-setwd("M:\\Anders L Kolstad\\systherb data\\PI_calibration")
+setwd("M:\\Anders L Kolstad\\R\\R_projects\\soilTemperature")
 sink("BLG.txt")
 cat("
 model{
-    #1A. Priors beta and sigma
-    for (i in 1:K) { beta[i] ~  dunif(0.0001, 100)}   
+    #1A. Priors beta and r
+    for (i in 1:K) { beta[i] ~  dunif(0, 50)}   
 
     #1B. Priors random effects 
     # not applicable
 
     #1C. Prior for r parameter of Gamma distribution
-    r ~ dunif(0, 10)
+    r ~ dunif(0, 1000)
 
     #2. Likelihood
     for (i in 1:N) {
       Y[i]        ~ dgamma(r, mu.eff[i])
       mu.eff[i]  <- r / mu[i]     
-      mu[i] <- eta[i]                        # removed log-link
-      eta[i]     <- inprod(beta[], X[i,])  
+      mu[i] <- inprod(beta[], X[i,]) }                      # removed log-link
+            
+
     #3. Discrepancy measures: Pearson residuals   
         for (i in 1:N) {
            VarY[i] <- mu[i]^2 / r
-           PRes[i] <- (Y[i] - eta[i]) / sqrt(VarY[i])   
-    }
-     }          
-}
+           PRes[i] <- (Y[i] - mu[i]) / sqrt(VarY[i])}
+    # back-standardice output
+        # skipping this in the model syntax this time
+         }
 ",fill = TRUE)
 sink()
 
 # Step 4: Initial values & parameters to save   # R syntax
 inits  <- function () {
   list(
-    beta  = runif(2, 0.001, 20),
+    beta  = runif(K, 0, 20),
     r     = runif(1, 0, 10))  }
 
 
 # Step 5: Specify what to save
 params <- c("beta", #Regression parameters
-            "r",
+            "r",    # gamma parameter
             "mu",
-            "PRes")   # gamma parameter
+            "PRes")  
 
 
 # Step 6: Start JAGS
@@ -300,4 +306,177 @@ MyBUGSChains(out,
 OUT1    <- MyBUGSOutput(out, 
                         c(uNames("beta", K), "r"),
                         VarNames = MyNames)
-print(OUT1, digits = 5)
+print(OUT1, digits = 5)  # based on standardised covaraites
+
+
+# Model validation:
+
+E1 <- out$mean$PRes
+F1 <- out$mean$mu
+
+par(mfrow = c(1, 1))
+plot(x = F1, 
+     y = E1,
+     xlab = "Posterior mean fitted values",
+     ylab = "Posterior mean Pearson residuals")
+abline(h = 0, lty = 2, col = 1)
+# Ok
+
+plot(y = E1, 
+     x = BLG$std.Broad_leaved_grasses)
+abline(h = 0, lty = 2)
+# Ok, same as above, just differently scaled axis
+
+
+
+#----------------------##
+# Model interpretation ####
+
+# Calculate the fitted values.
+# 1. Get the betas from the MCMC iterations
+# 2. Define a grid of covariate values
+# 3. Calculate the predicted values on this 
+#    grid for each MCMC iteration.
+# 4. Calculate the 95% credible intervals.
+# 5. Plot the whole thing
+
+
+# 1. Get the betas
+beta.mcmc <- out$sims.list$beta  #betas
+dim(beta.mcmc)  #15000 iterations saved - should be increased later...
+# Using these we can puzzle together the pieces.
+
+
+
+beta.mcmc2 <- beta.mcmc
+head(beta.mcmc)
+beta.mcmc[,2] <- (beta.mcmc2[,2] * sd(BLG$BLG_DW_g)) / sd(BLG$std.Broad_leaved_grasses)
+
+
+
+#2A. Define a grid of covariate values
+#   without extrapolation
+range(BLG$std.Broad_leaved_grasses)
+min(BLG$std.Broad_leaved_grasses)
+MyData <- data.frame(std.Broad_leaved_grasses = seq(from = min(BLG$std.Broad_leaved_grasses), 
+                                                    to = max(BLG$std.Broad_leaved_grasses), length = 50))
+head(MyData)
+
+#B. Convert the covariate values into an X matrix
+Xp <- model.matrix(~ std.Broad_leaved_grasses, 
+                   data = MyData) 
+
+
+
+#C. Calculate the predicted MCMC values
+# these are insenitive to standardisation of covariats (making the intercept and betas at the wrong scale)
+
+eta.mcmc <- Xp %*% t(beta.mcmc)     # model matrix * est.betas
+mu.mcmc  <- (eta.mcmc)    # removed exp function because there's not log-link
+
+
+
+# Why don't we take the 2.5% and 97.5% values
+# at each of the artificial covariate values?
+# And plot these instead of the 3,000 lines? 
+# We wrote  a small support function that does this:
+
+L <- GetCIs(mu.mcmc)
+L
+
+
+
+# GetCIs(): Support file to calculate the posterior
+# mean, se and 2.5 and 97.5 quantiles for 
+# fitted values. The input needs to be 
+# an observation - by - MCMC iteration object.
+
+# Each row in L is an artificial SDI value
+# The first column is the posterior mean of 
+# the MCMC iterations at that specific fitted 
+# value.
+# The second row is the SE, the third and fourth 
+# are the 2.5 and 97.5% quartiles and are used for 
+# the 95% credible interval. 
+
+# Now plot the data and draw lines for the mean,
+# and 2.5% and 97.5% values:
+
+MyData2 <- cbind(MyData,L)
+head(MyData2)
+
+# Back-standardize covariate
+MyData2$BLG_hits <- MyData$std.Broad_leaved_grasses * sd(BLG$Broad_leaved_grasses) + mean(BLG$Broad_leaved_grasses)
+
+
+
+
+
+
+
+p <- ggplot() +
+      geom_point(data = BLG, 
+                    aes(x = Broad_leaved_grasses, 
+                        y = BLG_DW_g))+
+  geom_line(data = MyData2, 
+                   aes(x = BLG_hits, 
+                       y = mean))+
+  geom_ribbon(data = MyData2,
+                     aes(x = BLG_hits, 
+                         ymax = up, 
+                         ymin = lo), 
+                     alpha = 0.5)+
+  xlab("Average number of hits per pin") + ylab("Dry weight (g)")+
+  theme(text = element_text(size=15)) +
+  theme(legend.position="none") +
+  ggtitle("Broad Leaved Grasses")+
+  theme(plot.title = element_text(hjust = 0.5))
+p
+
+
+pHist <- ggplot(data = as.data.frame(beta.mcmc), aes(x = beta.mcmc[,2]))+
+  geom_histogram(bins = 30)+
+  annotate("segment", x = mean(beta.mcmc[,2]), xend = mean(beta.mcmc[,2]), y = 0, yend = 2000,
+           colour = "blue", size = 1.5)+
+  annotate("segment", x = quantile(beta.mcmc[,2], probs = 0.975), xend = quantile(beta.mcmc[,2], probs = 0.975), 
+           y = 0, yend = 500,
+           colour = "red", size = 1.5)+
+  annotate("segment", x = quantile(beta.mcmc[,2], probs = 0.025), xend = quantile(beta.mcmc[,2], probs = 0.025), 
+           y = 0, yend = 500,
+           colour = "red", size = 1.5)+
+  annotate("text", x = 13, y = 1800, size = 5, label = "Mean = 9.4662")+
+  theme(text = element_text(size=15)) +
+  xlab("Estimated slope")
+pHist
+
+pHist_I <- ggplot(data = as.data.frame(beta.mcmc), aes(x = beta.mcmc[,1]))+
+  geom_histogram(bins = 30)+
+  annotate("segment", x = mean(beta.mcmc[,1]), xend = mean(beta.mcmc[,1]), y = 0, yend = 2000,
+           colour = "blue", size = 1.5)+
+  annotate("segment", x = quantile(beta.mcmc[,1], probs = 0.975), xend = quantile(beta.mcmc[,1], probs = 0.975), 
+           y = 0, yend = 500,
+           colour = "red", size = 1.5)+
+  annotate("segment", x = quantile(beta.mcmc[,1], probs = 0.025), xend = quantile(beta.mcmc[,1], probs = 0.025), 
+           y = 0, yend = 500,
+           colour = "red", size = 1.5)+
+  annotate("text", x = 13, y = 1800, size = 5, label = "Mean = 9.4893")+
+  theme(text = element_text(size=15)) +
+  xlab("Estimated intercept")
+pHist_I
+
+
+
+library(gridExtra)
+grid.arrange(arrangeGrob(p, ncol = 1, nrow=1), 
+             arrangeGrob(pHist, pHist_I, ncol = 2, nrow = 1))
+
+
+
+
+
+# back-transform intercept
+# test agains original data again
+par(mfrow = c(1,1), mar = c(5,5,5,2))
+plot(dat2$Broad_leaved_grasses, dat2$BLG_DW_g,  main = "BLG",  pch = 20) 
+summary(lm(dat2$BLG_DW_g~dat2$Broad_leaved_grasses))
+abline(lm(dat2$BLG_DW_g~dat2$Broad_leaved_grasses))
