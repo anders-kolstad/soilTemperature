11e71bfbaaf649433f06ee7ded3752f072365b48
Anders Lorentzen Kolstad <anderlk@ntnu.no>
2018-10-01 16:48:59 +0200

    Updated correactio matrix Figure S2

--- temperature script_new.R
+++ temperature script_new.R
@@ -2581,12 +2581,38 @@ Mypairs(SEMdat[,MyVars6])
 
 MyVarsFull <- c("CCI", "Moss_depth", "Soil_temp", "avenellaBM", "shrubBM",
                  "total_SR", "moss_SR", "vasc_SR")
-labs <- c("CCI", "Moss", "Temp.", "Avenella", "shrubs",
-          "total_SR", "moss_SR", "vasc_SR")
-#tiff("corr_matrix.tiff", units = "cm", res = 300, height = 40, width = 40)
-#Mypairs(SEMdat[,MyVarsFull])
-pairs(SEMdat[,MyVarsFull], labels = labs)
-#dev.off()
+labs <- c("CCI", "moss depth", "temp.", "Avenella", "shrubs",
+          "total SR", "moss SR", "vasc. SR")
+
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y, use = "pairwise.complete.obs"))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * r/2, col ="black")
+}
+
+panel.hist <- function(x, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "grey", ...)
+}
+
+#tiff("figS1_corrMat.tiff", units = "cm", res = 300, height = 30, width = 30)
+pairs(SEMdat[,MyVarsFull], gap = .35,
+      lower.panel = panel.smooth,
+      upper.panel = panel.cor,
+      diag.panel = panel.hist, labels = labs,
+      pch=1, col.smooth = "black", span = 0.5)
+
+
+
+dev.off()
 setwd("M:/Anders L Kolstad/R/R_projects/soilTemperature/")
 
 # Treatment is strongest on CCI, temp, vegetation heigh. 
